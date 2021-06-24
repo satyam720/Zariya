39fcfa1575c6508c8e8c231815d79f66396575fe
satyam1906 <psatyam1618@gmail.com>
2021-06-24 19:14:52 +0530

    Added single course view with details

--- client/pages/instructor/course/view/[slug].js
+++ client/pages/instructor/course/view/[slug].js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import InstructorRoute from "../../../../components/routes/InstructorRoute";
 import axios from 'axios';
+import {Avatar, Tooltip} from "antd";
+import { EditOutlined, CheckOutlined } from "@ant-design/icons";
 
 const CourseView = () => {
     const [course, setCourse] = useState();
@@ -21,7 +23,47 @@ const CourseView = () => {
     return (
         <InstructorRoute>
             <div className="container-fluid pt-3">
-            <pre>{JSON.stringify(course, null, 4)}</pre>
+            {/* <pre>{JSON.stringify(course, null, 4)}</pre>
+             */}
+             {course && 
+             <div className="container-fluid pt-1"> 
+                <div className="d-flex">
+                    <Avatar size={80} 
+                    src={course.image ? course.image.Location: '/course.png'}>
+
+                    </Avatar>
+                    <div className="pl-2 d-flex course-edit">
+                        <div className="row">
+                            <div className="col">
+                                <h5 className="mt-2 text-primary">
+                                    {course.name}
+                                </h5>
+                                <p style={{marginTop: "-10px"}}>{course.lessons && course.lessons.length} Lessons</p>
+                                <p style={{marginTop: '-15px', fontSize: "10px"}}>{course.category}</p>
+                    
+                                
+                    
+                            </div>
+                            
+                            <div className="mt-2  col button1-edit flex-end">
+                            <Tooltip title="Edit">
+                                <EditOutlined className="h5 pointer text-warning mr-4"/>
+                            </Tooltip>
+                            <Tooltip title="Publish">
+                                <CheckOutlined className="h5 pointer text-danger ms-3"/>
+                            </Tooltip>
+                        </div>
+                        
+                    
+                        </div>
+                       
+                        
+                    </div>
+
+                </div>
+             </div>
+             
+             }
 
             </div>
         </InstructorRoute>)
