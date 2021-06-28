a0ca93021c8d953f10b9d9394cd54a498d9d1f30
satyam1906 <psatyam1618@gmail.com>
2021-06-28 13:13:24 +0530

    published course client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,35 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+
+
 
 const Index = () => {
 
-    return (
-      <div>
-        <h1 className="jumbotron text-center bg-primary">Online Education MarketPlace</h1>
-        <p>from next</p>
+  const [courses, setCourses] = useState([]);
 
-      </div>
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const {data} = await axios.get("/api/courses");
+      setCourses(data);
+
+    };
+    fetchCourses();
+  }, []);
+
+
+    return (
+      <>
+        <h1 className="jumbotron text-center bg-primary">Online Education MarketPlace</h1>
+        
+        <div className="container-fluid "> 
+          <div className="row">
+            {courses.map((course) => <div key = {course._id} className="col-md-4"> 
+            {<pre>{JSON.stringify(course,null , 4)}</pre>}</div> )}
+          </div>
+        </div>
+      </>
       
       );
 };
