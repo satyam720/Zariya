b05fa04607245ea3386d975413c59ba381df66a8
satyam1906 <psatyam1618@gmail.com>
2021-06-28 21:33:24 +0530

    Fetch Single Course

---
+++ client/pages/course/[slug].js
@@ -0,0 +1,31 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import {useRouter} from 'next/router';
+
+const SingleCourse = ({course}) => {
+    const router = useRouter();
+    const {slug} = router.query;
+
+    return (
+        <>
+
+        <div className="container-fluid">
+            <div className="row">
+                <pre>{JSON.stringify(course, null, 4)}</pre>
+            </div>
+        </div>
+        </>);
+
+};
+
+
+export async function getServerSideProps({query}) {
+    const {data} = await axios.get(`${process.env.API}/course/${query.slug}`);
+    return {
+        props: {
+            course: data,
+        },
+    };
+};
+
+export default SingleCourse;
