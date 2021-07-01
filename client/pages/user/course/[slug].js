b064c9bf3ca3b1548290fd7fe4a01cbab380759a
satyam1906 <psatyam1618@gmail.com>
2021-07-01 13:58:34 +0530

    Lessons Menu Sidebar

--- client/pages/user/course/[slug].js
+++ client/pages/user/course/[slug].js
@@ -1,34 +1,116 @@
+// import { useState, useEffect } from "react";
+// import { useRouter } from "next/router";
+// import axios from 'axios';
+// import StudentRoute from '../../../components/routes/StudentRoute';
+// import {Button, Menu, Avatar} from 'antd';
+
+// const {Item } = Menu;
+
+
+// const SingleCourse = () => {
+
+//     const [clicked, setClicked] = useState(-1);
+//     const [collapsed,setCollapsed] = useState(false);
+
+//     const [loading ,setLoading] =useState(false);
+//     const [course, setCourse] = useState({lessons: []}); // course.lessons
+
+//     //router
+//     const router = useRouter();
+//     const {slug} = router.query;
+
+//     useEffect(() => {
+//         if (slug) loadCourse();
+//     },[slug]);
+
+//     const loadCourse = async () => {
+//         const {data } = await axios.get(`/api/user/course/${slug}`);
+//         setCourse(data);
+//     }
+
+//     return (
+//         <StudentRoute>
+//         <div className="row">
+//             <div style={{ maxWidth: 320}}>
+//                 <Menu 
+//                 defaultSelectedKeys={[clicked]}
+//                 inlineCollapsed={collapsed}
+//                 style={{height: '80vh', overflow: 'scroll'}}>
+//                     {course.lessons.map((lesson, index) => (
+//                         <Item onClick={() => setClicked(index)}
+//                         key={index}
+//                         icon={ <Avatar>{index+1}</Avatar>}>
+//                             {lesson.title.substring(0,30)}
+//                         </Item>
+//                     ))}
+//                 </Menu>
+//             </div>
+//         </div>
+
+//         </StudentRoute>)
+// }
+
+
+// export default SingleCourse;
+
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import axios from 'axios';
-import StudentRoute from '../../../components/routes/StudentRoute';
+import axios from "axios";
+import StudentRoute from "../../../components/routes/StudentRoute";
+import { Button, Menu, Avatar } from "antd";
 
-
-
+const { Item } = Menu;
 
 const SingleCourse = () => {
+  const [clicked, setClicked] = useState(-1);
+  const [collapsed, setCollapsed] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [course, setCourse] = useState({ lessons: [] });
 
-    const [loading ,setLoading] =useState(false);
-    const [course, setCourse] = useState({lessons: []}); // course.lessons
+  // router
+  const router = useRouter();
+  const { slug } = router.query;
 
-    //router
-    const router = useRouter();
-    const {slug} = router.query;
+  useEffect(() => {
+    if (slug) loadCourse();
+  }, [slug]);
 
-    useEffect(() => {
-        if (slug) loadCourse();
-    },[slug]);
+  const loadCourse = async () => {
+    const { data } = await axios.get(`/api/user/course/${slug}`);
+    setCourse(data);
+  };
 
-    const loadCourse = async () => {
-        const {data } = await axios.get(`/api/user/course/${slug}`);
-        setCourse(data);
-    }
+  return (
+    <StudentRoute>
+      <div className="row">
+        <div className="col-md-2" >
+          <Menu
+            defaultSelectedKeys={[clicked]}
+            inlineCollapsed={collapsed}
+            style={{ height: "80vh",  }}
+          >
+            {course.lessons.map((lesson, index) => (
+              <Item
+                onClick={() => setClicked(index)}
+                key={index}
+                icon={<Avatar>{index + 1}</Avatar>}
+              >
+                {lesson.title.substring(0, 30)}
+              </Item>
+            ))}
+          </Menu>
+        </div>
 
-    return (
-        <StudentRoute>
-            <h1> {JSON.stringify(course, null ,4)}</h1>
-        </StudentRoute>)
-}
-
+        <div className="col">
+          {clicked !== -1 ? (
+            <>{JSON.stringify(course.lessons[clicked])}</>
+          ) : (
+            <>Clcik on the lesson to start learning</>
+          )}
+        </div>
+      </div>
+    </StudentRoute>
+  );
+};
 
 export default SingleCourse;
