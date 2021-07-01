3d9875bf010ffa5883eb08f11cf4e902a89d7da7
satyam1906 <psatyam1618@gmail.com>
2021-07-01 11:41:55 +0530

    user Courses on dashboard

--- client/pages/user/index.js
+++ client/pages/user/index.js
@@ -1,7 +1,7 @@
-import { useContext } from "react";
+import {useState, useContext, useEffect} from "react";
 import { Context } from "../../context";
 import UserRoute from "../../components/routes/UserRoute";
-
+import axios from "axios";
 
 
 const UserIndex = () => {
@@ -10,6 +10,18 @@ const UserIndex = () => {
 //Create context
 const {state} = useContext(Context);
 const {user} = state;
+
+const [courses,setCourses] = useState([]);
+
+useEffect (() => {
+    loadCourses()
+},[]);
+
+const loadCourses = async () => {
+    const {data } = await axios.get('/api/user-courses');
+    <pre>{JSON.stringify(courses,null, 4)}</pre>
+    setCourses(data);
+}
 
 
  
